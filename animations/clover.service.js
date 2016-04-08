1eed92212ef33a83267f55c9a8fa54c2bd5b27ab
tylorspence <tylor.spence@conceptshare.com>
2016-04-07 22:53:19 -0400

    more work on clover animation

--- animations/clover.service.js
+++ animations/clover.service.js
@@ -9,21 +9,35 @@
 	function clover(genColors) {
 
 		var service = {
-			draw: draw
+			draw: draw,
+			mouseDownEvent: mouseDownEvent
 		};
 		var mainRadius = 150;
-		var dotSize = 10;
-		var numDots = 30;
+		var dotSize = 5;
+		var numDots = 55;
 		var angle = 0;
-		var speed = 0.0001;
-		var clrs = genColors.array.hex('#FF0000', '#0000FF', numDots);
-		var range = 0.2;
+		var speed = 0.0006;
+		var clrs = [];
+		var deadRange = 0.3;
+		var deadZones = 4;
 
 		//http://i.imgur.com/VlsPlbU.gif
+		service.mouseDownEvent({which:3});
 
 		return service;
 
 		//////////////////////////////////////////////////
+
+		function mouseDownEvent(e, state) {
+			if (e.which === 3) {
+				clrs = [];
+				var clr1 = genColors.random.hex();
+				var clr2 = genColors.random.hex();
+				for (var i = 0; i < numDots; i++) {
+					clrs.push(genColors.randomBetween.rgba(clr1, clr2, 1, 1))
+				}
+			}
+		}
 
 		function draw(ctx, state) {
 			ctx.clearRect(0,0,state.w, state.h);
@@ -31,25 +45,27 @@
 				angle += speed;
 				angle = angle % (Math.PI*2);
 				var rad = i/numDots * 2 * Math.PI;// + angle;
+				var mRad = mainRadius;
+				var inDeadZone = false;
 
-				if (angle > rad-range && angle < rad+range) {
-					ctx.fillStyle = '#000000';
+				for (var x = 0; x < deadZones; x++) {
+					var dzAngle = (Math.PI*2/deadZones*x)%(Math.PI*2) + angle;
+
+					if (dzAngle > rad-deadRange && dzAngle < rad+deadRange) {
+						ctx.fillStyle = '#FFFFFF';
+						inDeadZone = true;
+					}
 				}
-				else {
+				if(!inDeadZone) {
 					ctx.fillStyle = clrs[i];
 				}
-				ctx.strokeStyle = '#FFFFFF';
-				var mRad = mainRadius;
 
 				var x = mRad * Math.cos(rad) + state.xCenter;
 				var y = mRad * Math.sin(rad) + state.yCenter;
 
-
-
 				ctx.beginPath();
 				ctx.arc(x, y, dotSize, 0, Math.PI*2, true);
 				ctx.fill();
-				ctx.stroke();
 				ctx.closePath();
 
 			}
