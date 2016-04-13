520e782295df7b1070e680f79bc72a3bd5a3a971
tylorspence <tylor.spence@conceptshare.com>
2016-04-12 20:46:30 -0400

    disc service cleanup

--- animations/disc.service.js
+++ animations/disc.service.js
@@ -14,15 +14,15 @@
 		};
 
 		var angleSize = 0;
-		var discSlices = 20;
+		var discSlices = 30;
 		var discRows = 5;
 		var disc = [];
 		var verticalPadding = 20;
 		var rad;
 		var sliceAngles = [];
-		var initialized = false;
-		var hoverSample = -1;
-		var hoverBeat = -1;
+		var hoverRow = -1;
+		var hoverSlice = -1;
+
 		return service;
 
 		//////////////////////////////////////////////////
@@ -32,15 +32,14 @@
 			rad = state.h / 2 - (verticalPadding*2);
 			angleSize =  Math.PI * 2 / discSlices;
 			for (var i = 0; i < discRows; i++) {
-
 				var ring = {
 					index: i,
 					rad: rad / discRows * i,
-					beats: []
+					slice: []
 				};
 				for (var d = 0; d < discSlices; d++) {
 					sliceAngles[d] = angleSize * d;
-					ring.beats.push ({
+					ring.slice.push ({
 						a1: angleSize * d,
 						a2: angleSize * (d+1),
 						x: state.xCenter + (ring.rad) * Math.cos(angleSize * (d)),
@@ -50,30 +49,30 @@
 				disc.push(ring);
 			}
 			sliceAngles[discSlices] = angleSize * (d+1);
-			initialized = true;
 		}
 
 		function checkHoverPosition(state) {
 			var distanceFromCenter = Math.sqrt(
-				Math.pow(state.mouseX - (state.w / 2), 2) + Math.pow(state.mouseY - (state.h/2), 2) );
+				Math.pow(state.mouseX - (state.w / 2), 2) +
+				Math.pow(state.mouseY - (state.h / 2), 2) );
 			if (distanceFromCenter < rad) {
 				for (var layer = 0; layer < disc.length-1; layer++) {
 					if (distanceFromCenter > disc[layer].rad && distanceFromCenter < disc[layer+1].rad) {
-						hoverBeat = layer;
+						hoverSlice = layer;
 						break;
 					}
 				}
 				var angle = Math.atan2((state.h/2) - state.mouseY, state.w / 2 - state.mouseX) + Math.PI;
 				for (var i = 0; i < discSlices; i++) {
 					if (angle > sliceAngles[i] && angle < sliceAngles[i+1]) {
-						hoverSample = i;
+						hoverRow = i;
 						break;
 					}
 				}
 			}
 			else {
-				hoverSample = -1;
-				hoverBeat = -1;
+				hoverRow = -1;
+				hoverSlice = -1;
 			}
 		}
 
@@ -81,38 +80,36 @@
 			ctx.clearRect(0,0,state.w,state.h);
 			checkHoverPosition(state);
 
-			for (var sIndex = 0; sIndex < discSlices; sIndex++) {
-				for (var rIndex = 0; rIndex < discRows; rIndex++) {
+			for (var sliceIndex = 0; sliceIndex < discSlices; sliceIndex++) {
+				for (var rowIndex = 0; rowIndex < discRows; rowIndex++) {
 
-					var Sind2 = rIndex == discRows-1 ? 0 : rIndex + 1;
-					var bInd2 = sIndex == discSlices-1 ? 0 : sIndex + 1;
-					var p1 = disc[rIndex].beats[sIndex];
-					var p2 = disc[Sind2].beats[sIndex];
-					var p3 = disc[rIndex].beats[bInd2];
-					var p4 = disc[Sind2].beats[bInd2];
+					var rIndex = rowIndex == discRows-1 ? 0 : rowIndex + 1;
+					var sIndex = sliceIndex == discSlices-1 ? 0 : sliceIndex + 1;
+					var p1 = disc[rowIndex].slice[sliceIndex];
+					var p2 = disc[rIndex].slice[sliceIndex];
+					var p3 = disc[rowIndex].slice[sIndex];
+					var p4 = disc[rIndex].slice[sIndex];
 
 					ctx.beginPath();
 					ctx.moveTo(p1.x,p1.y);
 					ctx.lineTo(p2.x, p2.y);
-					ctx.arc(state.xCenter, state.yCenter, disc[Sind2].rad, p2.a1, p2.a2, false);
+					ctx.arc(state.xCenter, state.yCenter, disc[rIndex].rad, p2.a1, p2.a2, false);
 					ctx.lineTo(p3.x,p3.y);
-					ctx.arc(state.xCenter, state.yCenter, disc[rIndex].rad, p1.a2, p1.a1, true);
+					ctx.arc(state.xCenter, state.yCenter, disc[rowIndex].rad, p1.a2, p1.a1, true);
 
-
-
-					if (sIndex === hoverSample && hoverBeat === rIndex) { // hover cell
-						ctx.lineWidth = 3;
-						ctx.strokeStyle = "#000000";
-						ctx.fillStyle = "rgba(100,255,100,0.5)";
+					if (sliceIndex === hoverRow && hoverSlice === rowIndex) { // hover cell
+						ctx.lineWidth = 1;
+						ctx.strokeStyle = '#FFFFFF';
+						ctx.fillStyle = 'rgba(100,100,100,0.5)';
+						ctx.stroke();
+						ctx.fill();
 					}
 					else {
-						ctx.lineWidth = 1;
-						ctx.strokeStyle = "rgba(255,255,255,0.5)";
-						ctx.fillStyle = 'rgba(0,0,0,0)';
+						ctx.lineWidth = 0.2;
+						ctx.strokeStyle = 'rgba(255,255,255,0.5)';
+						ctx.stroke();
 					}
 
-					ctx.stroke();
-					ctx.fill();
 					ctx.closePath();
 				}
 			}
