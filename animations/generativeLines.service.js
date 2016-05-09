b1f254709870bcc4f9108d2d8ab46f2c3e945385
tylorspence <tylor.spence@conceptshare.com>
2016-05-08 20:11:26 -0400

    generative line service complete

--- animations/generativeLines.service.js
+++ animations/generativeLines.service.js
@@ -15,11 +15,14 @@
 		};
 
 		var lines = [];
-		var totalPoints = 20;
-		var numLines = 15;
+		var totalPoints = 40;
+		var numLines = 60;
+		var numLinesNoColor = 20;
 		var initialized = false;
-		var flux = 70;
-		var overallSpeed = 2;
+		var flux = 30;
+		var fluxGrowth = 3;
+		var overallSpeed = 4;
+
 		return service;
 
 		////////////////////////////////////////////////
@@ -29,14 +32,15 @@
 			var spacing = state.w / totalPoints;
 			for (var x = 0; x < numLines; x++) {
 				lines.push({
-					points: []
+					points: [],
+					flux: flux + ((numLines-x)*fluxGrowth)
 				});
-				for (var i = 0; i < totalPoints+1; i++) {
+				for (var i = 0; i <= totalPoints+1; i++) {
 					lines[x].points.push({
 						x: i * spacing,
 						y: 0,
 						f: genColors.get.randomNumber(-flux + 2, flux - 2, 0),
-						speed: genColors.get.randomNumber(0.1, 1)
+						speed: genColors.get.randomNumber(0.1, 1) * (Math.random()<.5 ? 1 : -1)
 					});
 				}
 			}
@@ -51,7 +55,6 @@
 					genColors.random.hex(),
 					lines.length
 				);
-				console.log(colors);
 				for (var x = 0; x < lines.length; x++) {
 					lines[x].c = colors[x];
 				}
@@ -61,34 +64,13 @@
 		function draw(ctx, state) {
 			if (!initialized) { service.initialize(state);}
 
-			//partial erase
-			var oldArray = ctx.getImageData(0,0,state.w,state.h);
-			for(var d=3;d<oldArray.data.length;d+=4){ //count through only the alpha pixels
-				//dim it with some feedback, I'm using .9
-				oldArray.data[d] = Math.floor(oldArray.data[d]*.9);
-			}
-			ctx.putImageData(oldArray,0,0);
-
-			// make point adjustments
-			for (var x = 0; x < lines.length; x++) {
-				for (var i = 0; i < lines[x].points.length; i++) {
-					var p = lines[x].points[i];
-					if (p.f > flux || p.f < -flux) {
-						p.speed *= -1;
-					}
-					p.f += (p.speed * overallSpeed);
-					p.y = state.h/2 + p.f;
-				}
-			}
-
-			// draw lines
-			for (x = 0; x < lines.length; x++) {
+			// draw lines in color
+			for (x = 0; x < lines.length - numLinesNoColor; x++) {
 				var line = lines[x];
 				ctx.strokeStyle = line.c;
 				ctx.beginPath();
-
 				ctx.moveTo(line.points[0].x, line.points[0].y);
-				for (i = 1; i < line.points.length-1; i++) {
+				for (i = 0; i < line.points.length-1; i++) {
 					var p1 = line.points[i];
 					var p2 = line.points[i+1];
 					var xc = (p1.x + p2.x) / 2;
@@ -98,6 +80,47 @@
 				ctx.stroke();
 				ctx.closePath();
 			}
+
+			// make point adjustments
+			for (var x = 0; x < lines.length; x++) {
+				line = lines[x];
+				for (var i = 0; i < line.points.length; i++) {
+					var p = line.points[i];
+					if (p.f > line.flux || p.f < -line.flux) {
+						p.speed *= -1;
+					}
+					p.f += (p.speed * overallSpeed);
+					p.y = state.h/2 + p.f;
+				}
+			}
+
+			//partial erase
+			var oldArray = ctx.getImageData(0,0,state.w,state.h);
+			for(var d=3;d<oldArray.data.length;d+=4){ //count through only the alpha pixels
+				//dim it with some feedback, I'm using .9
+				oldArray.data[d] = Math.floor(oldArray.data[d]*.93);
+			}
+			ctx.putImageData(oldArray,0,0);
+
+			// draw lines in white
+			for (x = 0; x < lines.length; x++) {
+				line = lines[x];
+				ctx.strokeStyle = '#FFFFFF';
+				ctx.beginPath();
+
+				ctx.moveTo(line.points[0].x, line.points[0].y);
+				for (i = 0; i < line.points.length-1; i++) {
+					p1 = line.points[i];
+					p2 = line.points[i+1];
+					xc = (p1.x + p2.x) / 2;
+					yc = (p1.y + p2.y) / 2;
+					ctx.quadraticCurveTo(p1.x, p1.y, xc, yc);
+				}
+				ctx.stroke();
+				ctx.closePath();
+			}
+
+
 		}
 	}
 })();
